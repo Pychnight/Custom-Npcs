d4bcad7f22afe04c7efbc9cabf08ec718bcf5894
TheWanderer <xenophobic.anon@gmail.com>
2014-03-16 21:48:12 -0400

    Make a proper plugin class now

--- CustomNPC/CustomNPC/CustomNPCPlugin.cs
+++ CustomNPC/CustomNPC/CustomNPCPlugin.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
 
 namespace CustomNPC
 {
-    public class CustomNPCPlugin
+    [ApiVersion(1, 15)]
+    public class CustomNPCPlugin : TerrariaPlugin
     {
+        public CustomNPCPlugin(Main game)
+            : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
     }
 }
