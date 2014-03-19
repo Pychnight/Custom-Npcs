69bf141656e74a8da5c4f281761c06fe3fc919f6
TheWanderer <xenophobic.anon@gmail.com>
2014-03-18 22:32:08 -0400

    Add an example npc plugin

---
+++ CustomNPC/TestNPC/TestNPCPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomNPC.EventSystem;
+using CustomNPC.Plugins;
+
+namespace TestNPC
+{
+    public class TestNPCPlugin : NPCPlugin
+    {
+        public TestNPCPlugin(IEventRegister register)
+            : base(register)
+        {
+        }
+
+        public override string Name
+        {
+            get { return "Test NPCs"; }
+        }
+
+        public override string[] Authors
+        {
+            get { return new[] { "TheWanderer", "IcyPhoenix" }; }
+        }
+
+        public override Version Version
+        {
+            get { return new Version(0, 1); }
+        }
+    }
+}
