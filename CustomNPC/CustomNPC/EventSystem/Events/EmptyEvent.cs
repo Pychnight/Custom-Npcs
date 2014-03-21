8222697f549a74aebfb96264932cf8f8de24eddf
TheWanderer <xenophobic.anon@gmail.com>
2014-03-20 20:20:45 -0400

    gg missing file?

---
+++ CustomNPC/CustomNPC/EventSystem/Events/EmptyEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNPC.EventSystem.Events
+{
+    public class PluginUpdateEvent
+    {
+        public static readonly PluginUpdateEvent Empty = new PluginUpdateEvent();
+    }
+}
