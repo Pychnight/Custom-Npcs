d34f0bf2961191de92ede39d7a3ddc5a7032d2ef
PhoenixICE <willson.fang@gmail.com>
2014-03-25 14:50:52 +1100

    Initial Commit for further spawn customization

---
+++ CustomNPC/CustomNPC/CustomNPCSpawning.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TShockAPI.DB;
+using TShockAPI;
+
+namespace CustomNPC
+{
+    class CustomNPCSpawning
+    {
+        public BiomeTypes spawnBiome { get; set; }
+        public Region spawnRegion { get; set; }
+        public int spawnRate { get; set; }
+        public double spawnChance { get; set; }
+        public List<SpawnConditions> spawnConditions { get; set; }
+        public CustomNPCSpawning(BiomeTypes spawnbiome, int spawnrate, List<SpawnConditions> spawnconditions, string spawnregion = "", double spawnchance = 100.0)
+        {
+            Region region = TShock.Regions.GetRegionByName(spawnregion);
+            if (region == null)
+            {
+                Log.ConsoleError("[CustomNPC]: Error Region \"{1}\" does not exist!", spawnregion);
+            }
+            spawnBiome = spawnbiome;
+            spawnRegion = region;
+            spawnRate = spawnrate;
+            spawnChance = spawnchance;
+            spawnConditions = spawnconditions;
+        }
+    }
+}
---
+++ CustomNPC/CustomNPC/SpawnConditions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomNPC
+{
+
+    /// <summary>
+    /// Spawn Conditions
+    /// </summary>
+    [Flags]
+    public enum SpawnConditions : byte
+    {
+        /// <summary>
+        /// Spawn anytime
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// Allowed in Day time (Main.dayTime == true)
+        /// </summary>
+        DayTime = 1,
+        /// <summary>
+        /// Allowed in Night time (Main.dayTime == true)
+        /// </summary>
+        NightTime = 2,
+        /// <summary>
+        /// Allowed in Eclipse
+        /// </summary>
+        Eclipse = 3,
+        /// <summary>
+        /// Allowed in Bloodmoon
+        /// </summary>
+        Bloodmoon = 4,
+        /// <summary>
+        /// Allowed when Raining
+        /// </summary>
+        Raining = 6,
+        /// <summary>
+        /// Allowed during day (Main.dayTime == true && Main.time => 150.0 && Main.time <= 26999.0)
+        /// </summary>
+        Day = 7,
+        /// <summary>
+        /// Allowed during night (Main.dayTime == false && Main.time => 0.0 && Main.16200)
+        /// </summary>
+        Night = 8,
+        /// <summary>
+        /// Allowed during noon (Main.dayTime == true && Main.time => 27000.0 && Main.time <= 54000)
+        /// </summary>
+        Noon = 9,
+        /// <summary>
+        /// Allowed during midnight (Main.dayTime == false && Main.time => 16200 && Main.time <= 32400)
+        /// </summary>
+        Midnight = 10
+    }
+}
